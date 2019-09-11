458caf523e97c6cc12fdc254a8308320469a00e3
eduardobastier93 <eduardobastier93@gmail.com>
2019-09-11 05:30:43 +0200

    Add common lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -12,5 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
- PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/aosp_potter.mk
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_potter.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_potter-userdebug \
+    aosp_potter-eng 
